765fb6801e661ec5bdd945441c2cd0502c3dea4f
Gnana Siva Sai V <gnana097@gmail.com>
2024-01-23 18:17:45 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gnana997/decentralised-storage-go
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("building decentralised storage system")
+}
